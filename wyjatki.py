14e165c972bf147ef0363cdfdf0da7c8c660e0a8
Zofia <zofia.miczynska@gmail.com>
2023-10-25 09:28:33 +0200

    addadddfhhdg

--- wyjatki.py
+++ wyjatki.py
@@ -21,14 +21,21 @@ while True:
         elif odp == '3':
             print(a*b)
         elif odp == '4':
-            #if b != 0:
-                print(a/b)
-            #else:
-            #print("Nie dziel przez zero")
+            #  if b != 0:
+            print(a / b)
+            #  else:
+            #  print("Nie dziel przez zero")
         else:
             print("Nie znam takiego dzialania")
-
     except ZeroDivisionError:
         print("Nie dziel przez zero")
-    except ValueError
+    except ValueError:
         print("Blad wartosci")
+    except TypeError:
+        print("Blad typu")
+    except Exception as e:
+        print("Wystapil blad", e)  # wystapil kazdy inny blad
+    else:
+        print("Tylko gdy nie ma bledow")
+    finally:
+        print("Wykonuje sie zawsze")  # drukuje sie niezaleznie od wystepowania bledu
