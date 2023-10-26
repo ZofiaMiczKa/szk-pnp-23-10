56099eb6148cfd793f622b764bb825681e38df47
Zofia <zofia.miczynska@gmail.com>
2023-10-26 15:30:34 +0200

    addadddfhhdg

--- kl2.py
+++ kl2.py
@@ -16,6 +16,12 @@ class Human:
     def podaj_plec(self):
         print(f'Plec {self.plec}')
 
+    def ruszaj(self):
+        if self.plec =="k":
+            print("Ruszylam w droge")
+        else:
+            print("Ruszylem w droge")
+
 cz1 = Human ("Ania",'39')
 print(cz1.wiek)
 print(cz1.plec)
@@ -23,3 +29,6 @@ print(cz1.imie)
 cz1.powitanie()
 cz1.podaj_wiek()
 cz1.podaj_plec()
+cz1.ruszaj()
+cz2 = Human("Radek", 48,"m")
+cz2.ruszaj()
